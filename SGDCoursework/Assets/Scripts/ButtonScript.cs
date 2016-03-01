085b8b3bf09ab25f72b8be6f38377213250e0445
ckinne1771 <ckinney1771@gmail.com>
2016-03-01 15:35:14 +0000

    Build buttons now change what is built
    Still a lot to do for other buttons logic and overall game logic.

---
+++ SGDCoursework/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+    public static bool BuildingButtonsActive = false;
+    public static bool cleanUpBool = false;
+
+    public static bool coalSelected = false;
+    public static bool oilSelected = false;
+    public static bool solarSelected = false;
+    public static bool windSelected = false;
+
+
+
+    public GameObject BuildingButton1;
+    public GameObject BuildingButton2;
+    public GameObject BuildingButton3;
+    public GameObject BuildingButton4;
+    // Use this for initialization
+
+    void Update()
+    {
+        if (cleanUpBool == true)
+        {
+            BuildingButtonsActive = false;
+            BuildingButton1.SetActive(false);
+            BuildingButton2.SetActive(false);
+            BuildingButton3.SetActive(false);
+            BuildingButton4.SetActive(false);
+            cleanUpBool = false;
+        }
+        
+    }
+
+
+
+    public void BuildingButtonsToggle()
+    {
+        if (BuildingButtonsActive == false)
+        {
+            BuildingButtonsActive = true;
+            BuildingButton1.SetActive(true);
+            BuildingButton2.SetActive(true);
+            BuildingButton3.SetActive(true);
+            BuildingButton4.SetActive(true);
+        }
+        else if(BuildingButtonsActive == true)
+        {
+            BuildingButtonsActive = false;
+            BuildingButton1.SetActive(false);
+            BuildingButton2.SetActive(false);
+            BuildingButton3.SetActive(false);
+            BuildingButton4.SetActive(false);
+        }
+    }
+
+    public void coalToggle()
+    {
+        if(coalSelected ==false)
+        {
+            coalSelected = true;
+        }
+        else if (coalSelected == true)
+        {
+            coalSelected = false;
+        }
+
+    }
+
+    public void oilToggle()
+    {
+        if (oilSelected == false)
+        {
+            oilSelected = true;
+        }
+        else if (oilSelected == true)
+        {
+            oilSelected = false;
+        }
+
+    }
+    public void solarToggle()
+    {
+        if (solarSelected == false)
+        {
+            solarSelected = true;
+        }
+        else if (solarSelected == true)
+        {
+            solarSelected = false;
+        }
+
+    }
+    public void windToggle()
+    {
+        if (windSelected == false)
+        {
+            windSelected = true;
+        }
+        else if (coalSelected == true)
+        {
+            windSelected = false;
+        }
+
+    }
+
+}
--- SGDCoursework/Assets/Scripts/InteractScript.cs
+++ SGDCoursework/Assets/Scripts/InteractScript.cs
@@ -5,6 +5,11 @@ public class InteractScript : MonoBehaviour
 {
 
     public Material IndustryMaterial;
+    public GameObject coalSprite;
+    public GameObject oilSprite;
+    public GameObject solarSprite;
+    public GameObject windSprite;
+    private GameObject spwanPosition;
     // Use this for initialization
     void Start()
     {
@@ -20,6 +25,41 @@ public class InteractScript : MonoBehaviour
             if (hit.collider.gameObject.tag == "emptyPanel")
             {
                 hit.collider.gameObject.GetComponent<Renderer>().material = IndustryMaterial;
+                string hitObject = hit.collider.gameObject.name;
+                //  Instantiate(coalSprite, new Vector3( this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+              if(this.gameObject.name == hitObject)
+                {
+                    if (ButtonScript.coalSelected == true)
+                    {
+                        Instantiate(coalSprite, new Vector3(this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+                        ButtonScript.coalSelected = false;
+                        ButtonScript.BuildingButtonsActive = false;
+                        ButtonScript.cleanUpBool = true;
+                    }
+                    if (ButtonScript.oilSelected == true)
+                    {
+                        Instantiate(oilSprite, new Vector3(this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+                        ButtonScript.oilSelected = false;
+                        ButtonScript.BuildingButtonsActive = false;
+                        ButtonScript.cleanUpBool = true;
+                    }
+                    if (ButtonScript.solarSelected == true)
+                    {
+                        Instantiate(solarSprite, new Vector3(this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+                        ButtonScript.solarSelected = false;
+                        ButtonScript.BuildingButtonsActive = false;
+                        ButtonScript.cleanUpBool = true;
+                    }
+                    if (ButtonScript.windSelected == true)
+                    {
+                        Instantiate(windSprite, new Vector3(this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+                        ButtonScript.windSelected = false;
+                        ButtonScript.BuildingButtonsActive = false;
+                        ButtonScript.cleanUpBool = true;
+                    }
+
+                }
+
             }
          
         }
