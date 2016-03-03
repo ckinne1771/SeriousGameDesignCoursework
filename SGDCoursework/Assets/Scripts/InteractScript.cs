655a9ec03e249eebf6126169adb70ce258efda79
ckinne1771 <ckinney1771@gmail.com>
2016-03-03 17:31:49 +0000

    Added changing variables. Comments added
    Comments added in Interact Script and Statistics Script (the only
    scripts being used, might need to do cleanup later). Variables mean
    something now, but still needs work done. TODO: Add next turn logic
    (Money generation, events, population changes, demand changes etc).

--- SGDCoursework/Assets/Scripts/InteractScript.cs
+++ SGDCoursework/Assets/Scripts/InteractScript.cs
@@ -4,55 +4,70 @@ using System.Collections;
 public class InteractScript : MonoBehaviour
 {
 
+    // Public variables for the interaction script. These hold a material which is used for changing the material of the background square on build
+    //and for holding sprites which represent the different buildings the player can build.
     public Material IndustryMaterial;
     public GameObject coalSprite;
     public GameObject oilSprite;
     public GameObject solarSprite;
     public GameObject windSprite;
     private GameObject spwanPosition;
-    // Use this for initialization
-    void Start()
-    {
-
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
+        //We are using raycasting for the interactions with the square. We only try to detect hits once we click the left mouse button
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
         if (Input.GetMouseButtonDown(0))
         {
+            //if the ray hits an empty panel TODO set panle with building on it to "filledPanel"
             if (hit.collider.gameObject.tag == "emptyPanel")
             {
+                //changes the material of the square
                 hit.collider.gameObject.GetComponent<Renderer>().material = IndustryMaterial;
+                //get the name of the hit square. We use the name of the object for detecting which panel was clicked. Without it, a building will be built on all squares (Yep, it was a bit funky).
                 string hitObject = hit.collider.gameObject.name;
-                //  Instantiate(coalSprite, new Vector3( this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+               
+                
               if(this.gameObject.name == hitObject)
                 {
+                  //if we are building a coal power plant
                     if (ButtonScript.coalSelected == true)
                     {
                         Instantiate(coalSprite, new Vector3(this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+                        StatisticsScript.EnergyOutput += 50;
+                        StatisticsScript.Money -= 100;
                         ButtonScript.coalSelected = false;
                         ButtonScript.BuildingButtonsActive = false;
                         ButtonScript.cleanUpBool = true;
                     }
+                  // if we are building an oil refinery
                     if (ButtonScript.oilSelected == true)
                     {
                         Instantiate(oilSprite, new Vector3(this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+                        StatisticsScript.EnergyOutput += 50;
+                        StatisticsScript.Money -= 100;
                         ButtonScript.oilSelected = false;
                         ButtonScript.BuildingButtonsActive = false;
                         ButtonScript.cleanUpBool = true;
                     }
+                  //if we are building a solar power plant
                     if (ButtonScript.solarSelected == true)
                     {
                         Instantiate(solarSprite, new Vector3(this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+                        StatisticsScript.EnergyOutput += 50;
+                        StatisticsScript.Money -= 100;
                         ButtonScript.solarSelected = false;
                         ButtonScript.BuildingButtonsActive = false;
                         ButtonScript.cleanUpBool = true;
                     }
+                  //if we are building a wind farm
                     if (ButtonScript.windSelected == true)
                     {
                         Instantiate(windSprite, new Vector3(this.transform.position.x, this.transform.position.y, -7), this.transform.rotation);
+                        StatisticsScript.EnergyOutput += 50;
+                        StatisticsScript.Money -= 100;
                         ButtonScript.windSelected = false;
                         ButtonScript.BuildingButtonsActive = false;
                         ButtonScript.cleanUpBool = true;
---
+++ SGDCoursework/Assets/Scripts/StatisticsScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StatisticsScript : MonoBehaviour {
+
+    //Public variables which represent the staticstics which change during the gameplay
+    public static int EnergyOutput;
+    public static int Population;
+    public static int EnergyDemand;
+    public static int Money;
+
+    //The Gaemobjects which represent the text output for the statistics
+    public GameObject EnergyOutputText;
+    public GameObject EnergyDemandText;
+    public GameObject PopulationText;
+    public GameObject MoneyText;
+
+	// At the start  of the game
+	void Start () {
+        EnergyDemand = 500;
+        EnergyOutput = 0;
+        Population = 1000;
+        Money = 1500;
+	}
+	
+	// Update is called once per frame. Changes the value of the statistics on screen.
+	void Update () {
+        EnergyDemandText.GetComponent<Text>().text = "Energy Demand: " + EnergyDemand.ToString();
+        EnergyOutputText.GetComponent<Text>().text = "Energy Output: " + EnergyOutput.ToString();
+        PopulationText.GetComponent<Text>().text = "Population: " + Population.ToString();
+        MoneyText.GetComponent<Text>().text = "Money: " + Money.ToString();
+        
+    }
+}
+
